abfa560c8c278f73c6486ff47ed9695b28df090f
Andras Mocsary <nobody@reedwolf.com>
2020-07-09 16:06:27 +0200

    Change Account's props to public

--- src/account.rs
+++ src/account.rs
@@ -6,16 +6,16 @@ use validator::Validate;
 
 #[cfg_attr(feature = "jsonschema", derive(JsonSchema))]
 #[cfg_attr(feature = "validate", derive(Validate))]
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Default)]
 pub struct Account {
     #[cfg(feature = "validate")]
     #[validate(email, non_control_character)]
-    pub(crate) email: String,
+    pub email: String,
     // Never leak this field
     #[serde(skip_serializing)]
     #[cfg(feature = "validate")]
     #[validate(length(min = 8), non_control_character)]
-    pub(crate) password: String,
+    pub password: String,
 
-    pub(crate) holder: Option<String>,
+    pub holder: Option<String>,
 }
